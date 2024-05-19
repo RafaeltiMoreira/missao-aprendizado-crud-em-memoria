3291733e867d8d9fff8802db4faf056630f0bd99
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-05-19 16:22:40 -0300

    Endpoint Read By ID '[GET] /personagem/:id' criado

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
-const lista = ['Java', 'Kotlin', 'Androoid']
+const lista = ['Java', 'Kotlin', 'Android']
 
 app.get('/personagem', function (req, res) {
-    res.send(lista)
+  res.send(lista)
+})
+
+app.get('/personagem/:id', function (req, res) {
+  const id = req.params.id
+  const item = lista[id - 1]
+  res.send(item)
 })
 
 app.listen(3000)
